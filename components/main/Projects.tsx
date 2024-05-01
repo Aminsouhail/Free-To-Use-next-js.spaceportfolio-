a4e576bb9d8fe6605f4d0044226b3c55bc8637cd
Aminsouhail <souhailamine97@gmail.com>
2024-05-01 03:19:05 +0100

    cards

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -1,35 +1,42 @@
 import React from "react";
 import ProjectCard from "../sub/ProjectCard";
 
+const projectsData = [
+  {
+    src: "/NextWebsite.png",
+    title: "Modern Next.js Portfolio",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    src: "/CardImage.png",
+    title: "Interactive Website Cards",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    src: "/SpaceWebsite.png",
+    title: "Space Themed Website",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+];
 
 const Projects = () => {
   return (
-    <div
-      className="flex flex-col items-center justify-center py-20"
-      id="projects"
-    >
+    <div className="flex flex-col items-center justify-center py-20" id="projects">
       <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
         My Projects
       </h1>
-      <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
-        <ProjectCard
-          src="/NextWebsite.png"
-          title="Modern Next.js Portfolio"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-          githubUrl="https://github.com/Aminsouhail/Myportfoli"
-        />
-        <ProjectCard
-          src="/CardImage.png"
-          title="Interactive Website Cards"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-          githubUrl="https://github.com/Aminsouhail/CardImageRepo"
-        />
-        <ProjectCard
-          src="/SpaceWebsite.png"
-          title="Space Themed Website"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-          githubUrl="https://github.com/Aminsouhail/SpaceWebsiteRepo"
-        />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {projectsData.map((project, index) => (
+          <ProjectCard
+            key={index}
+            src={project.src}
+            title={project.title}
+            description={project.description}
+          />
+        ))}
       </div>
     </div>
   );
